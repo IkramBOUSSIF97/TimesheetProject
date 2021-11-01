8eeb79a304bd72f73c84c4016bb79a795c89b575
Ikram-Boussif <ikram.boussif@esprit.tn>
2021-11-01 02:57:49 +0100

    Update RestControlEntreprise.java

--- src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEntreprise.java
@@ -31,6 +31,7 @@ public class RestControlEntreprise {
 	
 	// Ajouter Entreprise : http://localhost:8081/SpringMVC/servlet/ajouterEntreprise
 	//{"id":1,"name":"SSII Consulting","raisonSocial":"Cite El Ghazela"}
+	// tester
 
 	@PostMapping("/ajouterEntreprise")
 	@ResponseBody
