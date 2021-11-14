089c7e4cf8fe0836b93ca26ecb3f32ae1ea202ff
LOUATISOUNYA <sonia.louati1@esprit.tn>
2021-11-14 02:32:07 +0100

    Test - Log

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -6,68 +6,80 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import tn.esprit.spring.entities.Departement;
-import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Entreprise;
 import tn.esprit.spring.repository.DepartementRepository;
 import tn.esprit.spring.repository.EntrepriseRepository;
 
+
 @Service
 public class EntrepriseServiceImpl implements IEntrepriseService {
 
+	private static final Logger Log = LogManager.getLogger(EntrepriseServiceImpl.class);
 	@Autowired
-    EntrepriseRepository entrepriseRepoistory;
+	EntrepriseRepository entrepriseRepoistory;
 	@Autowired
 	DepartementRepository deptRepoistory;
-	
+
+//sonia
 	public int ajouterEntreprise(Entreprise entreprise) {
+		Log.info("debtu ajout");
 		entrepriseRepoistory.save(entreprise);
-		return entreprise.getId();
+		Log.info("fin ajout");
+	 return entreprise.getId();
 	}
 
+//ikram
 	public int ajouterDepartement(Departement dep) {
 		deptRepoistory.save(dep);
 		return dep.getId();
 	}
 	
+//sonia
 	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
-		//Le bout Master de cette relation N:1 est departement  
-				//donc il faut rajouter l'entreprise a departement 
-				// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
-				//Rappel : la classe qui contient mappedBy represente le bout Slave
-				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
-				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
-				Departement depManagedEntity = deptRepoistory.findById(depId).get();
-				
-				depManagedEntity.setEntreprise(entrepriseManagedEntity);
-				deptRepoistory.save(depManagedEntity);
-		
+		// Le bout Master de cette relation N:1 est departement
+		// donc il faut rajouter l'entreprise a departement
+		// ==> c'est l'objet departement(le master) qui va mettre a jour
+		// l'association
+		// Rappel : la classe qui contient mappedBy represente le bout Slave
+		// Rappel : Dans une relation oneToMany le mappedBy doit etre du cote
+		// one.
+		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+		Departement depManagedEntity = deptRepoistory.findById(depId).get();
+
+		depManagedEntity.setEntreprise(entrepriseManagedEntity);
+		deptRepoistory.save(depManagedEntity);
+
 	}
-	
+
+//ikram
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
 		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
 		List<String> depNames = new ArrayList<>();
-		for(Departement dep : entrepriseManagedEntity.getDepartements()){
+		for (Departement dep : entrepriseManagedEntity.getDepartements()) {
 			depNames.add(dep.getName());
 		}
-		
+
 		return depNames;
 	}
-
+	
+//sonia
 	@Transactional
 	public void deleteEntrepriseById(int entrepriseId) {
-		entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());	
+		entrepriseRepoistory.delete(entrepriseRepoistory.findById(entrepriseId).get());
 	}
-
+	
+//ikram
 	@Transactional
 	public void deleteDepartementById(int depId) {
-		deptRepoistory.delete(deptRepoistory.findById(depId).get());	
+		deptRepoistory.delete(deptRepoistory.findById(depId).get());
 	}
 
-
+//sonia
 	public Entreprise getEntrepriseById(int entrepriseId) {
-		return entrepriseRepoistory.findById(entrepriseId).get();	
+		return entrepriseRepoistory.findById(entrepriseId).get();
 	}
 
 }
--- src/main/java/tn/esprit/spring/test/EntrepriseServiceImplTest.java
+++ src/main/java/tn/esprit/spring/test/EntrepriseServiceImplTest.java
@@ -1,7 +1,79 @@
 package tn.esprit.spring.test;
 
-public class EntrepriseServiceImplTest {
-	//ajout classse
-	 
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+import java.util.Optional;
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.repository.DepartementRepository;
+import tn.esprit.spring.repository.EmployeRepository;
+import tn.esprit.spring.repository.EntrepriseRepository;
+import tn.esprit.spring.services.EntrepriseServiceImpl;
+import tn.esprit.spring.services.IEntrepriseService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntrepriseServiceImplTest {
+	
+	
+	 
+	@Mock
+	@Autowired
+	private EmployeRepository empR;
+	@Mock
+	@Autowired
+	private DepartementRepository depR;
+	@Mock
+	@Autowired
+	private EntrepriseRepository entR;
+	@Autowired
+	@InjectMocks
+	private EntrepriseServiceImpl entrS;
+	@Mock
+	@Autowired
+	private IEntrepriseService etrS;
+	private Entreprise e;
+	private Departement d;
+	
+	
+	@Before
+	public void setUp() {
+		e = entR.save(new Entreprise("Blue1", "Unity"));
+		d = depR.save(new Departement("GRH"));
+		d.setEntreprise(e);
+		depR.save(d);
+	}
+	
+	@After
+	public void tearDown() {
+		entR.deleteAll();
+		depR.deleteAll();
+		
+	}
+	
+	@Test
+	public void testAjouterDepartement() {
+		Departement dept = new Departement("TIC");
+		int AddedepID = entrS.ajouterDepartement(dept);
+		Optional<Departement> departement = depR.findById(AddedepID);
+		if(departement.isPresent()) {
+			String name= departement.get().getName();
+			assertEquals("TIC", name);
+		}
+	}
+	
+	@Test
+	public void  testajouterEntreprise(){
+		int id = etrS.ajouterEntreprise(new Entreprise("entreprise1","entreprise1")); 
+	    assertTrue("ajout Entreprise echoué",entR.findById(id).isPresent());
+	}
 }
